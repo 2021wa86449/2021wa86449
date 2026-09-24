ee4fbe26ad69f3b6df029ca4da9ca0b71911c41e
Divya Sarvani <2021wa86449@wilp-bits.pilani.ac.in>
2026-09-24 20:15:39 +0530

    adding pom.xml to github

---
+++ my-app/src/main/java/com/2021wa86449/app/App.java
@@ -0,0 +1,10 @@
+package com.2021wa86449.app;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ my-app/src/test/java/com/2021wa86449/app/AppTest.java
@@ -0,0 +1,19 @@
+package com.2021wa86449.app;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
